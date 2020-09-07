ed6933ebe4cfae9a278d72fdc5e87b7e09954bb6
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-09-07 10:57:55 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RouteGuard } from './guard/route.guard';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [RouteGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
